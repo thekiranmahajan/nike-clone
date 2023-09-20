d02a437bb0949a07a0fba945d36ebd032369860c
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-09-20 19:42:37 +0530

    did some changes to fix deployment

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,12 +2,18 @@ import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
-const Navbar = () => {
+const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} />
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
@@ -21,12 +27,17 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
         <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
         </div>
       </nav>
     </header>
   );
 };
 
-export default Navbar;
+export default Nav;
