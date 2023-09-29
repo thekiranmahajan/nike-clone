e9256cc9e56acdecae64dd9647526c7f1273c556
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-09-29 09:31:46 +0530

    added hamburger menu to Nav n some assets changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,56 @@
-import { hamburger } from "../assets/icons";
+import { useEffect, useState } from "react";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header
+      className={`padding-x py-8 fixed z-20 w-full ${
+        scrolled ? "scrolled" : ""
+      }`}
+    >
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul
+          id="navLinks"
+          className={`flex-1 flex justify-center items-center gap-16 max-lg:gap-10 max-md:flex-col max-md:absolute max-md:bg-white max-md:z-auto max-md:w-full max-md:right-16 max-md:items-end transition-all duration-500 ease-in ${
+            open ? "max-md:top-20" : "max-md:top-[-250px]"
+          }`}
+        >
           {navLinks.map((item) => (
-            <li key={item.label}>
+            <li key={item.label} onClick={() => setOpen(false)}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg text-slate-gray hover:text-black hover:font-medium"
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        <div
+          className="hidden max-md:block cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <i className={`fa-solid fa-${open ? "xmark" : "bars"} fa-2x`}></i>
         </div>
       </nav>
     </header>
