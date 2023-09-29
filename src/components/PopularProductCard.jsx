70b0758a911bbedb298f8e45404bc2bbe51e3071
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-09-29 09:40:39 +0530

    added card hover effect

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -3,7 +3,11 @@ import { star } from "../assets/icons";
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <img
+        src={imgURL}
+        alt={name}
+        className="w-[280px] h-[280px] hover:scale-90 transition-all duration-500 ease-in-out cursor-pointer"
+      />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,13 +1,13 @@
 const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
-    <div className="flex-1 sm:w-[350px] rounded-[20px] shadow-3xl px-10 py-16">
-      <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 hover:bg-coral-red hover:text-white cursor-pointer transition-all duration-500 ease-in-out">
+      <div className="w-11 h-11 bg-coral-red flex justify-center items-center rounded-full">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
-      <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">
+      <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold ">
         {label}
       </h3>
-      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray hover:text-slate-300">
         {subtext}
       </p>
     </div>
